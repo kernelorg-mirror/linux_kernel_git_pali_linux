d6aa4f4a901cf67fca75107b6781f5e088872560
Pali Rohár <pali@kernel.org>
2021-10-19 13:54:19 +0200

    PCI: aardvark: Add support for PCI Bridge Subsystem Vendor ID on emulated bridge
    Register with Subsystem Device/Vendor ID is at offset 0x2c. Export is via
    emulated bridge.
    
    After this change Subsystem ID is visible in lspci output at line:
    
      Capabilities: [40] Subsystem
    
    Signed-off-by: Pali Rohár <pali@kernel.org>

--- drivers/pci/controller/pci-aardvark.c
+++ drivers/pci/controller/pci-aardvark.c
@@ -35,6 +35,7 @@
 #define PCIE_CORE_DEV_ID_REG					0x0
 #define PCIE_CORE_CMD_STATUS_REG				0x4
 #define PCIE_CORE_DEV_REV_REG					0x8
+#define PCIE_CORE_SSDEV_ID_REG					0x2c
 #define PCIE_CORE_PCIEXP_CAP					0xc0
 #define PCIE_CORE_PCIERR_CAP					0x100
 
@@ -1180,6 +1181,8 @@ static int advk_sw_pci_bridge_init(struct advk_pcie *pcie)
 	/* Indicates supports for Completion Retry Status */
 	bridge->pcie_conf.rootcap = cpu_to_le16(PCI_EXP_RTCAP_CRSVIS);
 
+	bridge->subsystem_vendor_id = advk_readl(pcie, PCIE_CORE_SSDEV_ID_REG) & 0xffff;
+	bridge->subsystem_id = advk_readl(pcie, PCIE_CORE_SSDEV_ID_REG) >> 16;
 	bridge->has_pcie = true;
 	bridge->data = pcie;
 	bridge->ops = &advk_pci_bridge_emul_ops;
