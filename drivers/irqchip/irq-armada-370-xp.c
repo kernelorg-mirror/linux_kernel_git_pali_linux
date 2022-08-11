3cd8b83519729ac9a984576d5d15860e258e2003
Pali Rohár <pali@kernel.org>
2022-08-11 10:57:05 +0200

    irqchip/armada-370-xp: Do not call ipi_resume() when IPI is not used
    When IPI is not used then ipi_resume() does nothing. IPI is used only on
    systems where mpic controller does not parent GIC IRQ (e.g. on Armada XP).
    For documentation purpose add condition when ipi_resume() should be called.
    This simplify understanding of irq-armada-370-xp.c driver when individual
    driver functions are called when they do something. No functional change.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>

--- drivers/irqchip/irq-armada-370-xp.c
+++ drivers/irqchip/irq-armada-370-xp.c
@@ -528,7 +528,9 @@ static void armada_xp_mpic_reenable_percpu(void)
 		armada_370_xp_irq_unmask(data);
 	}
 
-	ipi_resume();
+	/* IPI is used only when we do not have parent irq */
+	if (parent_irq <= 0)
+		ipi_resume();
 
 	armada_370_xp_msi_reenable_percpu();
 }
@@ -747,7 +749,9 @@ static void armada_370_xp_mpic_resume(void)
 	if (doorbell_mask_reg & PCI_MSI_DOORBELL_MASK)
 		writel(1, per_cpu_int_base + ARMADA_370_XP_INT_CLEAR_MASK_OFFS);
 
-	ipi_resume();
+	/* IPI is used only when we do not have parent irq */
+	if (parent_irq <= 0)
+		ipi_resume();
 }
 
 static struct syscore_ops armada_370_xp_mpic_syscore_ops = {
